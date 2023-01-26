3055e5e4f94b8aa3cbd9cb3a85ed890b30b10ce0
kmichaeljy@gmail.com <kmichaeljy@gmail.com>
2023-01-27 00:00:05 +0900

    add reportStore

---
+++ stores/reportStore.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { makeObservable, observable, computed, action } from "mobx";
+
+/** 회원 로그인 상태 및 사용자 관련 상태 스토어 **/
+class ReportStore {
+    isEdited = false;
+
+    constructor() {
+        makeObservable(this, {
+            isEdited: observable,
+            setEditFlag: action
+        });
+    }
+    setEditFlag(state) {
+        this.isEdited = state;
+    }
+}
+const reportStore = new ReportStore();
+export default reportStore;
